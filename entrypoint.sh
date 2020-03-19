8394c47a0d5bccb2ed6bdaf1fad6f6e89fd179ef
odelucca <daniel.fonseca@quintoandar.com.br>
2020-03-19 17:34:08 -0300

    :fire: Removing unwanted debug code
    BLAC-1112
    
    After some testing, I've discovered that drone-s3 was bypassing the original
    decision tree to assume AWS creds.
    
    On this commit I'm fixing that, removing that bypass.
    
    Now we should be able to assume roles.

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,9 +6,6 @@ session_name=${AWS_SESSION_NAME:-${DRONE_REPO//\//-}}
 if [ -n "$role_arn" ]; then
     echo ">>> Assuming role..."
     sts=$(aws sts assume-role --role-arn "$role_arn" --role-session-name "$session_name" --query 'Credentials.[AccessKeyId,SecretAccessKey,SessionToken]' --output text)
-    aws configure list
-    echo 't'
-    aws sts get-caller-identity
     export AWS_ACCESS_KEY_ID=$(echo $sts | cut -d' ' -f1)
     export AWS_SECRET_ACCESS_KEY=$(echo $sts | cut -d' ' -f2)
     export AWS_SESSION_TOKEN=$(echo $sts | cut -d' ' -f3)
